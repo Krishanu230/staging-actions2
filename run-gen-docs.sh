05f23d7ecdd9a91bb099e9fccff7d2e69cbc65a3
Krishanu Singh <krishanu230@hotmail.com>
2023-04-26 06:42:35 +0530

    Update run-gen-docs.sh

--- run-gen-docs.sh
+++ run-gen-docs.sh
@@ -178,28 +178,4 @@ ls"
 
 #machine_type=""
 ls
-verify_wget
-verify_unzip
-verify_tmp
-check_knowl_cli_version
-cd $WORKING_DIR
-$BIN_UNZIP $WORKING_DIR/$KNOWL_CODE2DOC_NAME -d $WORKING_DIR
-cd s3
-cd code_to_doc
-pip install -r requirements.txt
-python3.9 python_docs.py -p $REPO_PATH -o $RESULT_DIR -l $FILES_DIFF
-ls $RESULT_DIR
-cat $RESULT_DIR/id_to_pages.json
-cd ..
-cd importer
-npm install -g typescript@4.8.4
-npm install --save-dev -g ts-node@10.9.1
-npm install --save node-fetch@2.6.2 @types/node-fetch@2.6.2
-npm install react react-dom @types/react-dom
-npm install --save lexical @lexical/react @lexical/headless @lexical/yjs 
-npm install --save jsdom ws dotenv
-npm install yjs y-websocket
-npm install --save cmd-ts
-npm i --save http-status-codes
-ts-node src/index.ts $RESULT_DIR
-ls
+env
